6773e51cf5934c2f33f42e2a12bcaa3b95469e45
gandlaf21 <gandlaf@proton.me>
2023-07-20 14:06:10 +0900

    details

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -29,7 +29,7 @@
 	};
 </script>
 
-<div class="flex m-2 gap-2 flex-col">
+<div class="flex m-2 gap-2 flex-col items-center">
 	<h1 class="text-center font-bold text-2xl">Money printer go brrrrr</h1>
 	<ul class="steps">
 		<li data-content="🏦" class="step step-primary" />
@@ -83,4 +83,10 @@
 			</button>
 		{/if}
 	</div>
+	<a href="https://gandlaf.com/donate" class="m-4 btn btn-sm btn-error btn-circle">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class=" w-6 h-6">
+			<path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+		  </svg>
+		</a>
+
 </div>
